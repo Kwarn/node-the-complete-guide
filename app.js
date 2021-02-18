7fc66abbf5088c3fddea652f667234054c7a889a
Kwarn <karlwarner.dev@gmail.com>
2021-02-18 11:00:09 +0000

    Section 6: Final clean up

--- app.js
+++ app.js
@@ -4,18 +4,11 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
-// const exphbs = require('express-handlebars');
 
 const app = express();
-// app.engine(
-//   'hbs',
-//   exphbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs',
-//   })
-// );
 app.set('view engine', 'ejs');
+
+// this is the default anyway
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +16,9 @@ app.use(express.static(path.join(rootDir, 'public')));
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes.routes);
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: '404 Error: Page Not Found' });
+  res
+    .status(404)
+    .render('404', { pageTitle: '404 Error: Page Not Found', path: '' });
 });
 
 app.listen(5501);
